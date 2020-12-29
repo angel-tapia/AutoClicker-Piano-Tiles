33243ebcd3aced5b35ea75a628d52efef0bfed45
angel-tapia <angeltapia1805@gmail.com>
2020-12-29 17:54:57 -0600

    Update click
    It will "predict" where it will appears the black rectangle when it see if it is at the top black

--- Clicker.py
+++ Clicker.py
@@ -5,7 +5,7 @@ import cv2
 from PIL import ImageGrab,Image 
 
 screenCoords = (515, 171, 783, 640)
-column = [489, 572, 652, 742]
+column = [620, 720, 810, 920]
 
 while True: 
     flag = False
@@ -15,16 +15,17 @@ while True:
     screen_np = cv2.cvtColor(screen_np, cv2.COLOR_BGR2GRAY)
     if(keyboard.is_pressed('q')):
         exit()
-    '''x, y = pyautogui.position()
+    '''
+    x, y = pyautogui.position()
     print(x, y)
     print(screen_np[x][y])
     if keyboard.is_pressed('s'):
         screen.show()
         exit()
     '''
-    for y in range(530, 173, -2):
+    for y in range(530, 100, -1):
         for x in column:
-            if screen_np[y][x] <= 10 and screen_np[y-25][x]==screen_np[y][x]:
+            if screen_np[y-60][x] <= 10:
                 pyautogui.click(x,y)
                 flag = True
                 break
@@ -32,7 +33,3 @@ while True:
                 exit()
         if flag is True:
             break      
-
-
-
-
